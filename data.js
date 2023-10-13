711ff8489ecc92c7379369eff20f2d83bc08e775
IanEddyKurniawan <ianedy@hyperdatasolusindo.com>
2023-10-13 13:26:51 +0700

    Contact person updates

---
+++ data.js
@@ -0,0 +1,40 @@
+const contact = {
+    'Antiokhia': [
+        {name: 'Jasmine', phone: '087888370605'},
+        {name: 'Kezia', phone: '089515961214'},
+        {name: 'Ivan', phone: '08179121102'},
+        {name: 'Ian', phone: '082192749859'},
+    ],
+    'Bethlehem': [
+        {name: 'Bindra', phone: '082234814818'},
+    ],
+    'Efrata': [
+        {name: 'Naomi', phone: '083833842921'},
+        {name: 'Nyimas', phone: '089508321714'},
+        {name: 'Ardo', phone: '0881036939298'},
+    ],
+    'Getsemani': [
+        {name: 'Ratih', phone: '085156231018'},
+        {name: 'David', phone: '08170901097'},
+        {name: 'Michele', phone: '082245550076'},
+    ],
+    'Filadelfia': [
+        {name: 'Tasha', phone: '081331823987'},
+    ],
+    'Volunteer': [
+        {name: 'David', phone: '08170901097'},
+        {name: 'Naomi', phone: '083833842921'},
+    ],
+}
+
+const images = {
+    'Antiokhia': 'https://lh3.googleusercontent.com/PMudLgnDynLCDpbRgEHMCoO68ugPvuTiwQKKWTTrimoQi8WrvoUgwnmDTKcxLQDOfn7raxzCURxsG15TdCGFj0QITcgSe-4SmO2ExDVV_zg-RkyFX8sV7FLoJSgNC8YCDW49AI1BnQ=w2400',
+    'Bethlehem': 'https://lh3.googleusercontent.com/pw/AJFCJaVR8phO98mr84Wtjzmy6gBHuvizFrmAL0cJUy06z8qaHHcg7j6pH_n2Wmy3PwEl_sxQ_D388pwibc2UkiXWYhXFgZknSXNuTsuEFiwaHFi_hSUGMfk=w2400',
+    'Efrata': 'https://lh3.googleusercontent.com/pw/AJFCJaUk9KGVyv_xYybjDCRSlL9pDjC0SINt-8dCvm72uQVC3hhakkZnbXAkFg1_EHAlXrO_tafYN7Vkd-fH4HLzlxhviTDaVmYJkFK6QJ3nMb2U2evI0ek=w2400',
+    'Getsemani': 'https://lh3.googleusercontent.com/pw/AJFCJaXl4le4F55ppQTAr5x_xljz5QSczvXsezI6WcT_yZmXXelJMfz7Tz9f14Vzdf-Fa1gjg7kbm5NO1R1_Mij8v3eE9-4cm9xBwsdqOVjRWPJOrvOJyL0=w2400',
+    'Filadelfia': 'https://lh3.googleusercontent.com/pw/AJFCJaXGpTyRCOIWlLvU2eRjS8Op1obxSjHpMLf0Q3YAocTamRP6TzR9_cSN95jNfuxzuWp_pGggLbdhTXdhNYwFdpxB2DVNCkxADx_baLpH6TTSt5FVQU0=w2400',
+    'Volunteer': 'https://lh3.googleusercontent.com/pw/AJFCJaWSi03BvhB8Wv0ZkbNjNY0OLQqLRvwkCmo0o_2Lg9PAudG_oFNU_5l-Hx7L1xun7MWEuKsQgfdURJXeN8vaftQ36xGNbuV7pxqD0XMfuKArtprcfgs=w2400',
+}
+
+
+        
